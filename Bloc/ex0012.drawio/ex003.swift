2ad287df184f0ad7c1d1e464cef54b57ee25eba0
Nikita <number_nik@mail.ru>
2022-07-17 13:21:31 +0300

    Добавили новую задачу

--- Bloc/ex0012.drawio/ex003.swift
+++ Bloc/ex0012.drawio/ex003.swift
@@ -4,5 +4,5 @@
 // var flag = n % 7 = 0 δδ n % 23 = 0
 // print(flag)
 
-var n = Int(readline()!)!            
+var n = Int(readline()!)!          
 print(n % 7 = 0 && n % 23 = 0)
---
+++ Bloc/ex0012.drawio/ex004.java
@@ -0,0 +1,12 @@
+public class ex004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("x0 = ");
+        int x0 = sc.nextInt();
+        System.out.printf("x1 = ");
+        int x1 = sc.nextInt();
+        double d = Math.abs(x1 - x0);
+        System.out.println(d);
+        sc.close();
+    }
+}
